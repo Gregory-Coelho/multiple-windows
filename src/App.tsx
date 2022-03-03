60fc96ad55ce21234e558194d38756f1e2030eb9
Gregory <gregory.coelho21@gmail.com>
2022-03-03 16:07:28 +0100

    nav bar position resolved

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,30 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import RenderTip from "./pages/tip-component/render-tip";
 import RenderPrice from "./pages/price-selector/render-price";
 
+import styles from "./styles/homePage.module.css";
+import "./styles/Global.css";
+
 function App() {
   const home = () => {
     return <h2>Home</h2>;
   };
 
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/tip-calculator">Tip Calculator</Link>
-            </li>
-            <li>
-              <Link to="/interactive-pricing">Price Selector</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className={styles.pageComponent}>
+      <BrowserRouter>
+        <div className={styles.navBar}>
+          <Link to="/">Home</Link>
+          <Link to="/tip-calculator">Tip Calculator</Link>
+          <Link to="/interactive-pricing">Price Selector</Link>
+        </div>
 
         <Routes>
           <Route path="/" element={home()} />
           <Route path="/tip-calculator" element={<RenderTip />} />
           <Route path="/interactive-pricing" element={<RenderPrice />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
